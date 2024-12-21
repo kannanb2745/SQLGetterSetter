9a957859382f9aa836543c832e8e750a11ebe891
kannanb2745 <kannan.b2745@gmail.com>
2024-12-21 12:32:49 +0530

    feat SQL version-o.2 [implemented insert and select]

--- SQLGetterSetter/setup.py
+++ SQLGetterSetter/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name = 'SQLGetterSetter',
-    version = '0.1.3',
+    version = '0.2',
     packages = find_packages(),
     install_requires =[
         #library like 'pymongo = 3.2.1',
