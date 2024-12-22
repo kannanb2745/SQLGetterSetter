a639b96af473bd95a72c063b83c888a48326b8a5
kannanb2745 <kannan.b2745@gmail.com>
2024-12-22 12:44:58 +0530

    feat sql->DISINT

--- SQLGetterSetter/build/lib/SQLGetterSetter/__init__.py
+++ SQLGetterSetter/build/lib/SQLGetterSetter/__init__.py
@@ -27,5 +27,21 @@ class MainClass:
         except Error as e:
             print(f"Error: {e}")
             self.connection.rollback()  # Rollback in case of error
+    def distinct(self, table, *args):
+        if not args:
+            raise ValueError("At least one column name must be provided for DISTINCT operation.")
+        
+        cursor = self.connection.cursor()
+        query = ', '.join(args)
+        
+        try:
+            cursor.execute(f"SELECT DISTINCT {query} FROM {table}")
+            rows = cursor.fetchall()
+            print("Data retrieved successfully:")
+            for row in rows:
+                print(row)
+        except Error as e:
+            print(f"Error: {e}")
 
+    
     
